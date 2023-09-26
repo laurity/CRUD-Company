365e2cadb7c1948c8b025604c661bd63f7d1bd04
Laura López <lauralopez166@gmail.com>
2023-09-26 20:59:57 +0200

    proyecto funcional

--- database/migrations/2023_09_26_174541_create_companies_table.php
+++ database/migrations/2023_09_26_174541_create_companies_table.php
@@ -12,12 +12,15 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {
-        Schema::create('companies', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('companies', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('email');
+        $table->string('address');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
